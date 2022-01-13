0dc4044b950229feecf3e149d22dda48d94ed3ce
leeyap <leeya.pressburger@pnnl.gov>
2022-01-13 15:47:31 -0500

    Progress 1-13
    Comments and small cleanup, added to dos

--- initial_steps_pt2.R
+++ initial_steps_pt2.R
@@ -1,42 +1,73 @@
-#comments
-#geom_bin for one year 2 vars, line per run
+# Parameter analysis round 2
+# Leeya Pressburger
+# January 2022
+
+# TO DO
+# Need an identifier in the output to know what beta/q10 values are being used
+
 library(hector)
 library(dplyr)
+library(ggplot2)
 
-# change to system.file
-rcp45 <- file.path("C:\\Users\\pres520\\Documents\\GitHub\\hector\\inst\\input\\hector_rcp45.ini")
+# Read in ini file, initiate new core
+rcp45 <- system.file("input/hector_rcp45.ini", package = "hector") 
+core <- newcore(rcp45)
 
+# Random number generator to allow for reproducibility 
 set.seed(10)
 
+# Create runlist of parameters of interest
 runlist <- tibble(
   run_number = 1:10,
   "BETA" = rnorm(10, mean = 0.36, sd = 0.036),
   "Q10_RH" = rnorm(10, mean = 2.0, sd = 0.2)
 )
 
+# Name and units of parameters
 name_vector <- c("BETA" = BETA(), "Q10_RH" = Q10_RH())
 units_vector <- c("BETA" = "(unitless)", "Q10_RH" = "(unitless)")
 
-run_hector <- function(pdata) {
-  core <- newcore(rcp45) # move outside, core param, reset core in loop, shutdown outside loop
+# Function to set parameter values, run Hector, and record tracking data output
+run_hector <- function(pdata, c) {
   for(p in colnames(pdata)) {
     message("Setting ", p)
-    #do.call(p, list()) to get rid of name_vector
-    setvar(core, NA, name_vector[p], pdata[p][[1]], units_vector[p])
+    setvar(c, NA, do.call(p, list()), pdata[p][[1]], units_vector[p])
   }
-  setvar(core, NA, TRACKING_DATE(), 1750, NA)
-  run(core)
-  get_tracking_data(core)
+  setvar(c, NA, TRACKING_DATE(), 1750, NA)
+  run(c)
+  tdata <- get_tracking_data(c)
+  # For now, just look at one year
+  tdata %>% filter(year %in% c(2100:2300))
 }
 
+# Run function for each row of the runlist
 output <- list()
 system.time({
   for(row in seq_len(nrow(runlist))) {
-    output[[row]] <- run_hector(runlist[row,][-1])
+    output[[row]] <- run_hector(runlist[row,][-1], core)
   }  
 })
 
+# Get output dataframe
+output <- bind_rows(output)
 
-# run_hector(runlist[1,][-1])
+#Data visualization
+#geom_bin for one year 2 vars, line per run
 
+# Not working - need identifier for different beta/q10 values to split up data
+ggplot(output, aes(year, source_fraction, fill = source_name)) +
+  geom_area() +
+  facet_wrap(~pool_name, scales = "free") +
+  theme_light()
+
+
+# Shutdown core
+shutdown(core)
+
+
+# Notes
 # record timings - compare
+# set core inside loop
+systemtime1 <- c("user" = 35.70, "system" = 0.56, "elapsed" = 36.28)
+# set core outside loop
+systemtime2 <- c("user" = 16.79, "system" = 0.05, "elapsed" = 16.84)
