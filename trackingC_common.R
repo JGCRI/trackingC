bda4b0c0a2dba5366054729935effed576e799ab
leeyap <leeya.pressburger@pnnl.gov>
2022-08-25 11:23:39 -0400

    10000 runs, update figures and tables

--- trackingC_common.R
+++ trackingC_common.R
@@ -23,13 +23,13 @@ scalar_vector <- "LUC_SCALE"
 
 # Number of runs for each SSP scenario
 SSP_runs <- c(#"ssp119" = 100,
-              "ssp126" = 1000,
-              "ssp245" = 1000,
-              "ssp370" = 1000,
+              "ssp126" = 2000,
+              "ssp245" = 2000,
+              "ssp370" = 2000,
               #"ssp434" = 100,
-              "ssp460" = 1000,
+              "ssp460" = 2000,
               #"ssp534-over" = 100,
-              "ssp585" = 1000)
+              "ssp585" = 2000)
 
 # We use GitHub Actions to make sure this RMarkdown knits successfully
 # But if running there, only do a small number of Hector simulations
