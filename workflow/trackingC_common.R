2cf83abaef904cee8c2523372d3964afc936be93
leeyap <leeya.pressburger@pnnl.gov>
2022-09-19 14:44:05 -0400

    Most recent testing, adjust aero

--- workflow/trackingC_common.R
+++ workflow/trackingC_common.R
@@ -23,13 +23,13 @@ scalar_vector <- "LUC_SCALE"
 
 # Number of runs for each SSP scenario
 SSP_runs <- c(#"ssp119" = 100,
-              "ssp126" = 500,
-              "ssp245" = 500,
-              "ssp370" = 500,
+              "ssp126" = 300,
+              "ssp245" = 300,
+              "ssp370" = 300,
               #"ssp434" = 100,
-              "ssp460" = 500,
+              "ssp460" = 300,
               #"ssp534-over" = 100,
-              "ssp585" = 500)
+              "ssp585" = 300)
 
 # We use GitHub Actions to make sure this RMarkdown knits successfully
 # But if running there, only do a small number of Hector simulations
