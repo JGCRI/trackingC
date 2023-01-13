5fbeab96a5ac31a657f02b98e61686d1437701a3
leeyap <leeya.pressburger@pnnl.gov>
2023-01-13 13:23:50 -0500

    Change param distributions to Hector defaults

--- workflow/common_data.R
+++ workflow/common_data.R
@@ -31,13 +31,13 @@ scalar_vector <- "LUC_SCALE"
 
 # Number of runs for each SSP scenario
 SSP_runs <- c(#"ssp119" = 100,
-              "ssp126" = 1000,
-              "ssp245" = 1000,
-              "ssp370" = 1000,
+              "ssp126" = 500,
+              "ssp245" = 500,
+              "ssp370" = 500,
               #"ssp434" = 100,
               #"ssp460" = 2000,
               #"ssp534-over" = 100,
-              "ssp585" = 1000)
+              "ssp585" = 500)
 
 # We use GitHub Actions to make sure this RMarkdown knits successfully
 # But if running there, only do a small number of Hector simulations
